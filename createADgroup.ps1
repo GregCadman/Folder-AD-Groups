750cab5c03393b63b5398ccb384dbff368705980
Gregory Cadman <42740815+GregCadman@users.noreply.github.com>
2019-03-25 08:34:49 -0400

    Add files via upload

--- createADgroup.ps1
+++ createADgroup.ps1
@@ -1,10 +1,11 @@
 ﻿$location = Read-Host -Prompt 'Enter Location of Folders'
 $domain = Read-Host -Prompt 'Enter domain name'
 $OU = Read-Host -Prompt 'Enter OU path (ex. OU=File Server,OU=Groups,dc=test,dc=com)'
+#Gather variable data
 
-get-childitem $location | ?{ $_.PSIsContainer } | Select-Object name | Export-Csv $location\departments.csv
+get-childitem $location | ?{ $_.PSIsContainer } | Select-Object name | Export-Csv $location\folders.csv
 
-$groupnames = Import-Csv $location\departments.csv
+$groupnames = Import-Csv $location\folders.csv
 
 foreach ($group in $groupnames) {
 $name = $group.Name 
@@ -14,8 +15,10 @@ foreach ($group in $groupnames) {
 $name = $group.Name 
 New-ADGroup -Name "$name -Read Only" -Path "$OU" -GroupScope Global -GroupCategory Security 
 }
+#Gather the names of the folders and create AD groups for all of them.
 
 Start-Sleep -Seconds 5
+#The script was running too fast before. It would attempt the next part before the AD groups were fully made. This lead to errors.
 
 Set-Location -Path $location
 
@@ -37,6 +40,7 @@ $objacl.AddAccessRule($objACEmod)
 
 (get-item $homeFolder).SetAccessControl($objacl)  
 }
+#Set modify rights for each folder for the respective AD group.
 
 foreach ($homeFolder in $homeFolders) {
 
@@ -50,3 +54,7 @@ $objacl.AddAccessRule($objACEread)
 
 (get-item $homeFolder).SetAccessControl($objacl)
 } 
+#Set read-only rights for each folder for the respective AD group.
+
+#Remove-Item -Path $location\folders.csv
+#Deletes the orginal csv file created earlier. Commented out incase you want to look at it for troubleshooting purposes.
